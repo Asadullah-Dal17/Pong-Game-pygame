0b049e610f8f46468ecbf2c2b1d1b59d78532da2
asadullah-dal-17 <asadullah.dal17@gmail.com>
2021-12-12 15:28:47 +0500

    create player, Ball and Opponets

--- main.py
+++ main.py
@@ -10,6 +10,13 @@ screen_height= 660
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Pong-game')
 
+# Game Rectangle 
+ball = pygame.Rect(screen_width/2-15, screen_height/2-15, 30,30)
+player = pygame.Rect(screen_width/-20, screen_height/2-70, 10,140)
+opponent = pygame.Rect(10, screen_height/2-70, 10, 140)
+bg_color = pygame.Color('gray12')
+light_gray = (200,200,200)
+
 while True:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
