18bcb86a6c88b31ff15cc2e548e1ac6001ac4555
asadullah-dal-17 <asadullah.dal17@gmail.com>
2021-12-12 16:35:17 +0500

    player animation

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
 import pygame 
 import sys 
+def ball_animation():
+    global ball_speed_x, ball_speed_y
+    # moveing the ball 
+    ball.x += ball_speed_x
+    ball.y += ball_speed_y
 
+    # limiting ball to go out of boundaries of screen 
+    if ball.top <=0 or ball.bottom >=screen_height:
+        ball_speed_y *= -1
+    if ball.left<=0 or ball.right>=screen_width:
+        ball_speed_x *= -1
+
+    # collision detection 
+    if ball.colliderect(player) or ball.colliderect(opponent):
+        ball_speed_x *= -1
+
+def player_animation():
+	player.y += player_speed
+
+	if player.top <= 0:
+		player.top = 0
+	if player.bottom >= screen_height:
+		player.bottom = screen_height
 pygame.init()
 
 clock = pygame.time.Clock()
@@ -11,6 +33,7 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Pong-game')
 ball_speed_x = 5 
 ball_speed_y = 5
+player_speed =0
 # Game Rectangle 
 ball = pygame.Rect(screen_width/2-15, screen_height/2-15, 30,30)
 player = pygame.Rect(screen_width-20, screen_height/2-70, 10,140)
@@ -23,17 +46,21 @@ while True:
         if event.type==pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type ==pygame.KEYDOWN:
+            if event.key ==pygame.K_DOWN:
+                player_speed +=5
+                print('keey down')
+            if event.key ==pygame.K_UP:
+                player_speed -=5
+        if event.type == pygame.KEYUP:
+            if event.key ==pygame.K_DOWN:
+                player_speed -=5
+            if event.key ==pygame.K_UP:
+                player_speed +=5
+                
+    player_animation()
         
-    # moveing the ball 
-    ball.x += ball_speed_x
-    ball.y += ball_speed_y
-    
-    # limiting ball to go out of boundaries of screen 
-    if ball.top <=0 or ball.bottom >=screen_height:
-        ball_speed_y *=-1
-    if ball.left<=0 or ball.right>=screen_width:
-        ball_speed_x *=-1
-
+    ball_animation()
     # Draw Visuals 
 
     screen.fill(bg_color)
